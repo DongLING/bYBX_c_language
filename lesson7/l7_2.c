77baca619ac0192013d5145395ccef555eac3706
Dong LING <canada.dongling@gmail.com>
2015-06-05 14:17:08 -0400

    Debug: l7-4.c in lesson7 folder.
    Change 'i' to 'j' in for(j = 0; i < 3; i++); statement, because it has a deadlock without change.

--- lesson7/l7_2.c
+++ lesson7/l7_2.c
@@ -1,18 +1,21 @@
  /*----------------------------------------------------------
   程序L7_2.C功能：输入10个整数，输出最大数。
-  ----------------------------------------------------------*/
- #include <stdio.h>
- void main()    
- {
-  int i,array[10],big;
-  /*给数组中所有元素赋值  */
-  for(i=0;i<10;i++) 
-     scanf("%d",&array[i]);
-  /*找出数组中最大的元素 */
-  big=array[0];
-  for(i=0;i<10;i++) 
-        if(array[i]>big) big=array[i];
-  printf ("The biggest is %3d\n",big);
- }
+  ----------------------------------------------------------*/  
+#include <stdio.h>
 
-
+int main() 
+{
+	int i, array[10], big;
+	
+    /*给数组中所有元素赋值  */ 
+	printf("Use a 'space' key for separat the numbers inputted, please!\n");
+    for(i = 0; i < 10; i++) scanf("%d", &array[i]);
+	
+    /*找出数组中最大的元素 */ 
+    big = array[0];
+	/* The 'for' statement + the 'if' statement + assignment statement */
+	for(i = 0; i < 10; i++) if(array[i] > big) big = array[i];
+	printf("The biggest number is: %d.\n", big);
+
+	return 0;
+}
--- lesson7/l7_3.c
+++ lesson7/l7_3.c
@@ -1,19 +1,28 @@
  /*-----------------------------------------------------------
   程序L7_3.C功能：一维数组初始化的几种形式。
- -------------------------------------------------------------*/
- #include <stdio.h>
- void main()    
- { 
-  int n;
-  int a[5]={0,1,2,3,4};
-  int b[5]={1,2};
-  float c[ ]={1,2,3,4,5};
-  for (n=0;n<5;n++)    printf("%5d",a[n]);
-  printf("\n");
-  for (n=0;n<5;n++)    printf("%5d",b[n]);
-  printf("\n");
-  for (n=0;n<5;n++)    printf("%5.1f",c[n]);
-  printf("\n");
- }
+ -------------------------------------------------------------*/  
+/*
+ * "%s/\r//g" command is used for delete '^M' in a file.
+ * The name of "{}" is "curly bracket" or "brace".
+ */
 
-
+#include <stdio.h>
+
+int main() 
+{
+	int n;
+	int a[5] = { 0, 1, 2, 3, 4 };
+	int b[5] = { 1, 2 };
+	float c[] = { 1, 2, 3, 4, 5 };
+
+	for (n = 0; n < 5; n++)	printf("%5d", a[n]);
+	printf("\n");
+
+	for (n = 0; n < 5; n++)	printf("%5d", b[n]);
+	printf("\n");
+
+	for (n = 0; n < 5; n++)	printf("%5.1f", c[n]);
+	printf("\n");
+
+	return 0;
+}
